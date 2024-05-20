534d75c94e5695ed03474b73e5933e01032ed14a
unknown <mohamedelafandy593@gmail.com>
2024-05-20 09:40:36 -0700

    forbidden error hundler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,19 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes=False)
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> None:
-    """GET /api/v1/unauthorized
+    """ GET /api/v1/unauthorized
     Return:
-      - Unauthorized error.
+      - unauthorized error
     """
     abort(401)
 
 
-@app_views.route('/forbidden/', strict_slashes=False)
-def forbidden() -> None:
-    """GET /api/v1/forbidden
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
+    """ GET /api/v1/forbidden
     Return:
-      - Forbidden error.
+      - forbidden error
     """
     abort(403)
